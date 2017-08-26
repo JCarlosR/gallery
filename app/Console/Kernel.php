706cbfa918091dc29f53dd33fb4947a4ea5972a5
Juan Carlos <juancagb.17@gmail.com>
2017-08-26 18:54:03 -0500

    Clear L log every 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')->everyMinute();
+        $schedule->exec('echo "" > /var/www/gallery/storage/logs/laravel.log')->everyFiveMinutes();
     }
 
     /**
